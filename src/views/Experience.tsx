532e30b53f2756f92a3f0825b1e119227e72ac19
Joaquim Merlo <joaquimmerlo1@gmail.com>
2022-11-09 22:03:24 -0400

    finished

--- src/views/Experience.tsx
+++ src/views/Experience.tsx
@@ -1,9 +1,80 @@
 import { PorfolioLayout } from "../layout/PorfolioLayout"
+import './Experience.css'
 
 export const Experience = () => {
     return (
         <PorfolioLayout className="home experience">
-            <h1>Experiencia</h1>
+            <div className="textExperience">
+                <h1>Experience</h1>
+                <p>I have experience in these branches:</p>
+            </div>
+
+
+            <div className="skillsExperience">
+                <div className="skills-bar">
+                    <div className="bar">
+                        <div className="info">
+                            <span>React</span>
+                        </div>
+                        <div className="progress-line html"><span></span></div>
+                    </div>
+                    <div className="bar">
+                        <div className="info">
+                            <span>Redux</span>
+                        </div>
+                        <div className="progress-line jquery"><span></span></div>
+                    </div>
+                    <div className="bar">
+                        <div className="info">
+                            <span>Node</span>
+                        </div>
+                        <div className="progress-line node"><span></span></div>
+                    </div>
+
+                    <div className="bar">
+                        <div className="info">
+                            <span>Express</span>
+                        </div>
+                        <div className="progress-line python"><span></span></div>
+                    </div>
+                    <div className="bar">
+                        <div className="info">
+                            <span>Mongo</span>
+                        </div>
+                        <div className="progress-line mysql"><span></span></div>
+                    </div>
+                    <div className="bar">
+                        <div className="info">
+                            <span>html</span>
+                        </div>
+                        <div className="progress-line html2"><span></span></div>
+                    </div>
+                    <div className="bar">
+                        <div className="info">
+                            <span>css</span>
+                        </div>
+                        <div className="progress-line css"><span></span></div>
+                    </div>
+                    <div className="bar">
+                        <div className="info">
+                            <span>javascript</span>
+                        </div>
+                        <div className="progress-line javascript"><span></span></div>
+                    </div>
+                    <div className="bar">
+                        <div className="info">
+                            <span>typescript</span>
+                        </div>
+                        <div className="progress-line typescript"><span></span></div>
+                    </div>
+                    <div className="bar">
+                        <div className="info">
+                            <span>git-github</span>
+                        </div>
+                        <div className="progress-line github"><span></span></div>
+                    </div>
+                </div>
+            </div>
         </PorfolioLayout>
     )
 }
--- src/views/Greeting.tsx
+++ src/views/Greeting.tsx
@@ -14,6 +14,40 @@ export const Greeting = () => {
                     <h2>web developer</h2>
                 </div>
             </div>
+            <div className='cohete'>
+                <div className="containerFlame">
+                    <div className="wrapper flame1">
+                        <div className="flame-wrapper">
+                            <div className="flame red"></div>
+                            <div className="flame orange"></div>
+                            <div className="flame gold"></div>
+                            <div className="flame white"></div>
+                            <div className="base blue"></div>
+                            <div className="base black"></div>
+                        </div>
+                    </div>
+                    <div className="wrapper flame2">
+                        <div className="flame-wrapper">
+                            <div className="flame red"></div>
+                            <div className="flame orange"></div>
+                            <div className="flame gold"></div>
+                            <div className="flame white"></div>
+                            <div className="base blue"></div>
+                            <div className="base black"></div>
+                        </div>
+                    </div>
+                    <div className="wrapper flame3">
+                        <div className="flame-wrapper">
+                            <div className="flame red"></div>
+                            <div className="flame orange"></div>
+                            <div className="flame gold"></div>
+                            <div className="flame white"></div>
+                            <div className="base blue"></div>
+                            <div className="base black"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
